46c25adc0a74701b7ff5e7529bf3f61ebd63c562
Artem <metra52@ya.ru>
2019-02-10 21:39:31 +0300

    New version

--- ctl_to_msq/Form1.Designer.cs
+++ ctl_to_msq/Form1.Designer.cs
@@ -36,6 +36,11 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -66,14 +71,14 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
             this.textBox2.Size = new System.Drawing.Size(323, 22);
-            this.textBox2.TabIndex = 3;
+            this.textBox2.TabIndex = 2;
             // 
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(342, 74);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(242, 23);
-            this.button2.TabIndex = 2;
+            this.button2.TabIndex = 3;
             this.button2.Text = "УОЗ от дросселя";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
@@ -84,14 +89,14 @@
             this.textBox3.Name = "textBox3";
             this.textBox3.ReadOnly = true;
             this.textBox3.Size = new System.Drawing.Size(323, 22);
-            this.textBox3.TabIndex = 5;
+            this.textBox3.TabIndex = 4;
             // 
             // button3
             // 
             this.button3.Location = new System.Drawing.Point(342, 103);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(242, 23);
-            this.button3.TabIndex = 4;
+            this.button3.TabIndex = 5;
             this.button3.Text = "Состав смеси от дросселя";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
@@ -100,15 +105,62 @@
             // 
             this.richTextBox1.Location = new System.Drawing.Point(12, 132);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(776, 293);
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(776, 101);
             this.richTextBox1.TabIndex = 8;
             this.richTextBox1.Text = "";
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(649, 97);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(139, 28);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Convert to .table";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "Выберите папку для сохранения калибровок";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(649, 68);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(139, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Save Folder";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 240);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Папка результатов конвертации:\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(238, 240);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 12;
+            // 
             // Form1
             // 
+            this.AcceptButton = this.button5;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 278);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button3);
@@ -117,7 +169,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Cte to Table converter";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -133,6 +185,11 @@
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- ctl_to_msq/Form1.cs
+++ ctl_to_msq/Form1.cs
@@ -5,7 +5,11 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
 using System.IO;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace ctl_to_msq
@@ -13,18 +17,177 @@ namespace ctl_to_msq
     public partial class Form1 : Form
     {
         List<string> cteFileAddr = new List<string>();
-        string uozFileName = null;
         string filtercte = "Калибровки ChipTuner(*.cte)|*.cte";
         string baseermessage = "Не верный файл калибровки ";
         string errdownlmes = "Ошибка загрузки";
+        string savefolder;
+        //Для формирования XML файла, разметка
+        string[] RPMValue = new[] { "600.0", "800.0", "1000.0", "1200.0", "1600.0", "2000.0", "2520.0", "3000.0", "3520.0", "4000.0", "4520.0", "5000.0", "5520.0", "6000.0", "7000.0", "8000.0" };
+        string[] TPSLoad = new[] { "0.00", "2.00", "4.00", "6.00", "8.00", "10.00", "14.00", "18.00", "23.00", "29.00", "37.00", "46.00", "56.00", "66.00", "80.00", "100.00" };
+        double[,] mass = new double[16, 16];
 
         public Form1()
         {
             InitializeComponent();
+            foreach (Button butt in Controls.OfType<Button>())
+            {
+                if (butt.TabIndex != 10) {
+                    butt.Enabled = false;
+                }
+            }
+        }
+
+        public void parceCTE(TextBox txt) //Функция разбора файла калибровки ChiptunerPRO
+        {
+            StreamReader cte = new StreamReader(txt.Text, Encoding.Default);
+            Regex regex = new Regex(@"^X([0-9]+)Z([0-9]+)=(.*)$", RegexOptions.Compiled);
+            Regex names = new Regex(@"^Name=(.*)$", RegexOptions.Compiled);
+            MatchCollection matches = null;
+            MatchCollection namemat = null;
+            string CalName = null;
+            while (cte.EndOfStream == false)
+            {
+                string a = cte.ReadLine();
+                matches = regex.Matches(a);
+                namemat = names.Matches(a);
+                if (matches.Count > 0)
+                {
+                    foreach (Match match in matches)
+                    {
+                        mass[Convert.ToInt32(match.Groups[2].Value)-1, Convert.ToInt32(match.Groups[1].Value)-1] = Convert.ToDouble(match.Groups[3].Value);
+                        // richTextBox1.AppendText(match.Groups[1] +" " +match.Groups[2] + Environment.NewLine);
+                    }
+                }
+
+                if (namemat.Count > 0)
+                {
+                    foreach (Match name in namemat)
+                    {
+                       // richTextBox1.AppendText(Tr(name.Groups[1].Value)+" - Successfully"+Environment.NewLine);
+                        CalName = Tr(name.Groups[1].Value);
+                    }
+                }
+            }
+            xmlcreate(CalName);
+            // richTextBox1.AppendText(xmlzValuesmass(mass)); //Проверка строк\столбцов                -сюда выводится
+
+        }
+
+        private static string xmlzValuesmass(double[,] ms,string name) //Создание массива данных zValues
+        {
+            string stroka = null;
+            double[,] bit = new double[16, 16];
+            string[,] bitst = new string[16, 16];
+            for (var i=0; i<16; i++)
+            {
+                stroka = stroka + Environment.NewLine+ new string(' ', 9);
+                for (var j = 0; j < 16; j++)
+                {
+                    if (name == "Popravka CN ot drosselya")
+                    {
+                        bit[i, j] = Math.Round(ms[i, j] * 100, 2);
+                    }
+                    else
+                    {
+                        bit[i, j] = Math.Round(ms[i, j], 2);
+                    }
+                    int count = BitConverter.GetBytes(decimal.GetBits((decimal)bit[i, j])[3])[2];
+                    bitst[i, j] = Convert.ToString(bit[i, j]);
+                    Regex pattern = new Regex("[ , ]"); //ПОд вопросом
+                    bitst[i, j] = pattern.Replace(bitst[i, j], ".");
+                    if (count < 2)
+                        {
+                            if (count < 1)
+                            {
+                                stroka = stroka + bitst[i, j] + ".00 ";
+                            }
+                            else
+                            {
+                                stroka = stroka + bitst[i, j] + "0 ";
+                            }
+                        }
+                    else
+                        {
+                                stroka = stroka + bitst[i, j] + " ";
+                        }
+                }
+            }
+            return stroka; 
+        }
+
+        public void xmlcreate(string name) //Создание xml
+        {
+            //SaveFolder
+            string fileName = savefolder + "/" + name + ".table";
+            /////////////////////////
+            XDocument doc = new XDocument(new XDeclaration("1.0", "UTF-8", "no"));
+            XElement tableData = new XElement("tableData");
+            doc.Add(tableData);
+
+
+            XElement bibliography = new XElement("bibliography");
+            
+            bibliography.Add(new XAttribute("author", "EFI Analytics - philip.tobin@yahoo.com"),
+                new XAttribute("company", "EFI Analytics, copyright 2010, All Rights Reserved."),
+                new XAttribute("writeDate", Convert.ToString(DateTime.UtcNow)));
+
+
+            XElement versionInfo = new XElement("versionInfo");
+            versionInfo.Add(new XAttribute("fileFormat", "1.0"));
+
+            XElement table = new XElement("table");
+            table.Add(new XAttribute("cols", "16"),
+                      new XAttribute("rows", "16")
+                );
+           //структура данных оси Х (обороты)
+            XElement xAxis = new XElement("xAxis");
+            xAxis.Add(new XAttribute("cols", "16"),
+                      new XAttribute("name", "RPMValue"));
+            //Заменить на Массив
+            for (var i = 0; i < RPMValue.Count();i++)
+            {
+                xAxis.Value = xAxis.Value + Environment.NewLine + new string(' ', 9) + RPMValue[i]; //Формирование строки новым способом
+                if(i == RPMValue.Count()-1)
+                {
+                    xAxis.Value = xAxis.Value + Environment.NewLine + new string(' ', 4);
+                }
+            }
+           // xAxis.Value = xAxis.Value + Environment.NewLine + new string(' ', 9); //Формируем конец строки
+            table.Add(xAxis);
+
+            //структура данных оси Y (нагрузка/Дроссель)
+            XElement yAxis = new XElement("yAxis");
+            yAxis.Add(new XAttribute("name", "LoadEngine"),
+                      new XAttribute("rows", "16"));
+            for (var i = 0; i < TPSLoad.Count(); i++)
+            {
+                yAxis.Value = yAxis.Value + Environment.NewLine + new string(' ', 9) + TPSLoad[i]; //Формирование строки новым способом
+                if (i == TPSLoad.Count() - 1)
+                {
+                    yAxis.Value = yAxis.Value + Environment.NewLine + new string(' ', 4);
+                }
+            }
+           // yAxis.Value = yAxis.Value + Environment.NewLine + new string(' ', 9); //Формируем конец строки
+            table.Add(yAxis);
+            //структура данных оси Z (Числовые значения)
+            XElement zValues = new XElement("zValues");
+            zValues.Add(new XAttribute("cols", "16"),
+                      new XAttribute("rows", "16"));
+            zValues.Value = xmlzValuesmass(mass,name) + Environment.NewLine + new string(' ', 4); //вот тут вот  уже mass весь обнулен
+            table.Add(zValues);
+            // )));
+
+            doc.Root.Add(bibliography);
+            doc.Root.Add(versionInfo);
+            doc.Root.Add(table);
+
+            doc.Save(fileName);
+            richTextBox1.AppendText(name + " - Successfully" + Environment.NewLine);
+
         }
 
         //Проверка на валидность
-        public bool check(string filename,string text)
+        private bool check(string filename,string text)
         {
             StreamReader sr = new StreamReader(filename, Encoding.Default);
             string name = null;
@@ -33,13 +196,16 @@ namespace ctl_to_msq
             {
                 name = sr.ReadLine();
                 count++;
-               
             }
-            if (name.Contains(text)) { return true; } else { return false; }
+            if (name != null)
+            {
+                if (name.Contains(text)) { return true; } else { return false; }
+            }
+            else { return false; }
 
         }
         //Открытие файла и запись адреса
-        public void openfile(string name,TextBox txt,int number) {
+        private void openfile(string name,TextBox txt,int number) {
             OpenFileDialog pcn = new OpenFileDialog();
             string FileAddr = null;
             pcn.Filter = filtercte;
@@ -47,18 +213,20 @@ namespace ctl_to_msq
             if (pcn.ShowDialog() == DialogResult.OK)
             {
                 FileAddr = pcn.FileName;
-
-            }
-
             if (check(FileAddr, name) == true)
             {
                 cteFileAddr.Add(pcn.FileName);
                 txt.Text = FileAddr;
-                txt.ReadOnly = false;
+               // txt.ReadOnly = false;
             }
             else
             {
                 MessageBox.Show(baseermessage+name, errdownlmes, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            }
+            else
+            {
+                MessageBox.Show("Не выбран файл калибровки", errdownlmes, MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
 
@@ -79,11 +247,144 @@ namespace ctl_to_msq
             openfile(button3.Text, textBox3, 2);
 
 //Проверка наличия адресов в списке
-            richTextBox1.Clear();
-                    foreach (string l in cteFileAddr)
-                    richTextBox1.AppendText(l+" ");
+            //richTextBox1.Clear();
+                   // foreach (string l in cteFileAddr)
+                   // richTextBox1.AppendText(l+" ");
 //////////////////////////////////////////
               
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (transliter.Count == 0)
+            {
+                prepareTranslit();
+            }
+            var count = 0;
+            foreach (TextBox textBox in Controls.OfType<TextBox>())
+            {
+                if (textBox.Text == "")
+                {
+                    if (count < 1)
+                    {
+                        MessageBox.Show("Отсутствующие калибровки не будут сконвертированы ", errdownlmes, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        count++;
+                    }
+                }
+                else
+                {
+
+                    if (textBox.TabIndex < 5)
+                    {
+                        parceCTE(textBox);
+                    }
+                    
+                }
+            }
+            richTextBox1.AppendText(Environment.NewLine);
+        }
+
+        public static string Tr(string sourceText)
+        {
+            StringBuilder ans = new StringBuilder();
+            for (int i = 0; i < sourceText.Length; i++)
+            {
+                if (transliter.ContainsKey(sourceText[i].ToString()))
+                {
+                    ans.Append(transliter[sourceText[i].ToString()]);
+                }
+                else
+                {
+                    ans.Append(sourceText[i].ToString());
+                }
+            }
+            return ans.ToString();
+        }
+
+        private static Dictionary<string,string> transliter = new Dictionary<string, string>();
+        private static void prepareTranslit() //Трансляция в транслит названия
+        {
+            transliter.Add("а", "a");
+            transliter.Add("б", "b");
+            transliter.Add("в", "v");
+            transliter.Add("г", "g");
+            transliter.Add("д", "d");
+            transliter.Add("е", "e");
+            transliter.Add("ё", "yo");
+            transliter.Add("ж", "zh");
+            transliter.Add("з", "z");
+            transliter.Add("и", "i");
+            transliter.Add("й", "j");
+            transliter.Add("к", "k");
+            transliter.Add("л", "l");
+            transliter.Add("м", "m");
+            transliter.Add("н", "n");
+            transliter.Add("о", "o");
+            transliter.Add("п", "p");
+            transliter.Add("р", "r");
+            transliter.Add("с", "s");
+            transliter.Add("т", "t");
+            transliter.Add("у", "u");
+            transliter.Add("ф", "f");
+            transliter.Add("х", "h");
+            transliter.Add("ц", "c");
+            transliter.Add("ч", "ch");
+            transliter.Add("ш", "sh");
+            transliter.Add("щ", "sch");
+            transliter.Add("ъ", "j");
+            transliter.Add("ы", "i");
+            transliter.Add("ь", "j");
+            transliter.Add("э", "e");
+            transliter.Add("ю", "yu");
+            transliter.Add("я", "ya");
+            transliter.Add("А", "A");
+            transliter.Add("Б", "B");
+            transliter.Add("В", "V");
+            transliter.Add("Г", "G");
+            transliter.Add("Д", "D");
+            transliter.Add("Е", "E");
+            transliter.Add("Ё", "Yo");
+            transliter.Add("Ж", "Zh");
+            transliter.Add("З", "Z");
+            transliter.Add("И", "I");
+            transliter.Add("Й", "J");
+            transliter.Add("К", "K");
+            transliter.Add("Л", "L");
+            transliter.Add("М", "M");
+            transliter.Add("Н", "N");
+            transliter.Add("О", "O");
+            transliter.Add("П", "P");
+            transliter.Add("Р", "R");
+            transliter.Add("С", "S");
+            transliter.Add("Т", "T");
+            transliter.Add("У", "U");
+            transliter.Add("Ф", "F");
+            transliter.Add("Х", "H");
+            transliter.Add("Ц", "C");
+            transliter.Add("Ч", "Ch");
+            transliter.Add("Ш", "Sh");
+            transliter.Add("Щ", "Sch");
+            transliter.Add("Ъ", "J");
+            transliter.Add("Ы", "I");
+            transliter.Add("Ь", "J");
+            transliter.Add("Э", "E");
+            transliter.Add("Ю", "Yu");
+            transliter.Add("Я", "Ya");
+        }
+
+        private void button5_Click(object sender, EventArgs e) //SaveFolder
+        {
+            folderBrowserDialog1.SelectedPath = Environment.CurrentDirectory;
+            folderBrowserDialog1.ShowDialog();
+            savefolder = folderBrowserDialog1.SelectedPath;
+            foreach (Button butt in Controls.OfType<Button>())
+            {
+                if (butt.TabIndex != 10)
+                {
+                    butt.Enabled = true;
+                }
+            }
+            label2.Text = savefolder;
         }
     }
 }
